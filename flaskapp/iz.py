24d0208bf807ea5a1069aa181d44dcf37a05b03d
DenDen438 <85965174+DenDen438@users.noreply.github.com>
2021-06-24 09:08:25 +0700

    Update iz.py

--- flaskapp/iz.py
+++ flaskapp/iz.py
@@ -54,7 +54,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 ## функция для обработки изображения 
-def draw(filename,cho):
+def change(filename,cho):
  ##открываем изображение 
  print(filename)
  image= Image.open(filename)
@@ -133,7 +133,7 @@ def net():
   ch=form.cho.data
  
   form.upload.data.save(filename)
-  newfilename,grname = draw(filename,ch)
+  newfilename,grname = change(filename,ch)
  # передаем форму в шаблон, так же передаем имя файла и результат работы нейронной
  # сети если был нажат сабмит, либо передадим falsy значения
  
